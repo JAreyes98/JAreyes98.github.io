eefe12995ee7bcb83bbc3279d2449fb4eeb9f5c7
JAreyes98 <josuereyeschevy@gmail.com>
2026-02-13 14:18:13 -0800

    Avoid indexing errors 5xx

--- middleware.tsx
+++ middleware.tsx
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/request'
+import type { NextRequest } from 'next/server'
 import { match } from '@formatjs/intl-localematcher'
 import Negotiator from 'negotiator'
 
@@ -9,7 +9,6 @@ let defaultLocale = 'en'
 function getLocale(request: NextRequest) {
   const negotiatorHeaders: Record<string, string> = {}
   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
-
   const languages = new Negotiator({ headers: negotiatorHeaders }).languages()
   return match(languages, locales, defaultLocale)
 }
@@ -20,7 +19,7 @@ export function middleware(request: NextRequest) {
   if (
     pathname === '/sitemap.xml' || 
     pathname === '/robots.txt' || 
-    pathname.includes('.')
+    pathname.includes('.') 
   ) {
     return NextResponse.next()
   }
@@ -37,7 +36,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: [
-    '/((?!api|_next/static|_next/image|images|favicon.ico|sitemap.xml|robots.txt|.*\\..*).*)',
-  ],
+  matcher: ['/((?!api|_next/static|_next/image|images|favicon.ico|sitemap.xml|robots.txt|.*\\..*).*)'],
 }
