968fa1326688a75f08d5fa0f3968664582a2432e
JAreyes98 <josuereyeschevy@gmail.com>
2026-02-13 13:39:47 -0800

    Add robots and sitemap.xml into the middleware

--- middleware.tsx
+++ middleware.tsx
@@ -29,6 +29,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!api|_next/static|_next/image|images|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|images|robots.txt|sitemap.xml|favicon.ico).*)',
   ],
 }
